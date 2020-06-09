24500363661df7f9312f27579cce217a7fcea2df
jonahkiprop34@gmail.com <jonahkiprop34@gmail.com>
2020-06-09 21:01:25 +0300

    #use body-parser to parse req-res

--- src/endpoints/index.js
+++ src/endpoints/index.js
@@ -2,7 +2,9 @@ const timeouturl = require('./timeout.url');
 const resulturl = require('./result.url');
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json({urlencoded: true}));
 router.use('/b2c/timeout',timeouturl);
 router.use('/b2c/result', resulturl);
 
